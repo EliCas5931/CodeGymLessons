64f16653ee31cba8aa4bf37cd768e0c0d5581024
Elicas5931 <elizabeth.perry.339@gmail.com>
2020-03-04 23:52:42 -0500

    Adding instructions first

---
+++ CodeGym/src/main/java/LevelTwo/MinimumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package LevelTwo;
+
+public class MinimumOfTwoNumbers {
+
+//    Write a function that returns the minimum of two numbers.
+//
+//    Hint:
+//    You need to write the body of the existing min function.
+//
+//            Requirements:
+//            •	The program should display text on the screen.
+//            •	The min method should not display text on the screen.
+//            •	The main method should call the min method three times.
+//            •	The main method should display the result of the min method. Each time, on a new line.
+//            •	The min method must return the minimum of the numbers a and b.
+
+    public static void main(String[] args) {
+
+
+    }
+}
