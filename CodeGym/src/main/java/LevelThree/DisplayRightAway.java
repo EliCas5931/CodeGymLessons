cda782eb73e4f9cb4ddecf7c88476daa34481309
Elicas5931 <elizabeth.perry.339@gmail.com>
2020-03-12 23:19:50 -0400

    Adding first few lessons from Level 3

---
+++ CodeGym/src/main/java/LevelThree/DisplayRightAway.java
@@ -0,0 +1,26 @@
+package LevelThree;
+
+public class DisplayRightAway {
+
+//    Add the public static void printString(String s) method, and write code that will make it display the passed string.
+//
+//    Requirements:
+//            •	Add a printString method that takes a String argument.
+//            •	The printString method must be void.
+//            •	The printString method must be static.
+//            •	The printString method must be public.
+//            •	The printString method must display the passed text.
+//            •	The program should display "Hello, Amigo!".
+//
+// Your code goes here:
+    
+    
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+
+    public static void printString(String s) {
+        System.out.println(s);
+    }
+
+}
