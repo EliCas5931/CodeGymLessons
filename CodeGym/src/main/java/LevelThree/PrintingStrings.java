e5d676b8e48a4d13ff86dfa0ff8bc044e9395f4f
Elicas5931 <elizabeth.perry.339@gmail.com>
2020-03-17 09:42:33 -0400

    Adding a few more lessons

---
+++ CodeGym/src/main/java/LevelThree/PrintingStrings.java
@@ -0,0 +1,28 @@
+package LevelThree;
+
+public class PrintingStrings {
+
+//    Implement the public static void writeToConsole(String s) method, which adds "printing: " to the beginning of the string and displays the altered string.
+//
+//    Example output for "Hello, World! ":
+//    printing: Hello, World!
+//
+//    Requirements:
+//            •	Add a writeToConsole method that takes a String argument.
+//            •	The writeToConsole method must be void.
+//            •	The writeToConsole method must be static.
+//            •	The writeToConsole method must be public.
+//            •	The writeToConsole method should add "printing: " to the beginning of the string and display the result.
+//            •	The program should display "printing: Hello, World!".
+
+//    Code given:
+
+    public static void main(String[] args) {
+        writeToConsole("Hello, World!");
+    }
+
+    public static void writeToConsole(String s) {
+        //write your code here
+    }
+
+}
---
+++ CodeGym/src/main/java/LevelThree/TimeConversion.java
@@ -0,0 +1,21 @@
+package LevelThree;
+
+public class TimeConversion {
+
+//    Add a public static int convertToSeconds(int hour) method that converts hours to seconds.
+//    Call it twice in the main method with any arguments.
+//    Display the results, each time on a new line.
+//
+//            Requirements:
+//            •	Add the convertToSeconds method. It must be public static, and take and return ints.
+//            •	The convertToSeconds method must convert the passed number of hours to seconds.
+//            •	The main method must call the convertToSeconds method twice.
+//            •	The main method should display the result of the calls, each time on a new line.
+//            •	The convertToSeconds method should not display anything.
+
+    public static void main(String[] args) {
+
+        
+    }
+
+}
